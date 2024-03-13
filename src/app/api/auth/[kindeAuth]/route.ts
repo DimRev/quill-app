0687ed9f48c19744f5f133a8d939ba97c6d54560
dimrev <dimarev444@gmail.com>
2024-03-13 20:38:05 +0200

    Disable es-lint check on file

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
 import { NextRequest } from 'next/server'
 
@@ -5,3 +6,4 @@ export async function GET(request: NextRequest, { params }: any) {
   const endpoint = params.kindeAuth
   return handleAuth(request, endpoint)
 }
+/* eslint-enable */
