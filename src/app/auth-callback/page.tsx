9320b4930d16184de9500a3967c51b53d6db0c79
dimrev <dimarev444@gmail.com>
2024-03-13 21:16:43 +0200

    fix suspense in auth-callback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useRouter, useSearchParams } from 'next/navigation'
-import React, { Suspense, useEffect } from 'react'
+import React, { Suspense } from 'react'
 import { trpc } from '../_trpc/client'
 import { Loader2 } from 'lucide-react'
 
@@ -12,34 +12,26 @@ const Page = (props: Props) => {
 
   const origin = searchParams.get('origin')
 
-  const { data, error, isSuccess, isError } = trpc.authCallback.useQuery(
-    undefined,
-    {
-      retry: true,
-      retryDelay: 500,
-    }
-  )
-
-  useEffect(() => {
-    if (isSuccess) {
+  const { data, error } = trpc.authCallback.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
+    onSuccess: (data) => {
       const success = data.success
-      if (success) router.push(origin ? `/${origin}` : 'dashboard')
-    } else if (isError) {
-      const err = error
-      if (err.data?.code === 'UNAUTHORIZED') router.push('/sign-in')
-    }
-  }, [isSuccess, isError, data, error, origin, router])
+      if (success) router.push(origin ? `/${origin}` : '/dashboard')
+    },
+    onError: (error) => {
+      if (error.data?.code === 'UNAUTHORIZED') router.push('/sign-in')
+    },
+  })
 
   return (
-    <Suspense>
-      <div className="mt-24 flex w-full justify-center">
-        <div className="flex flex-col items-center gap-2">
-          <Loader2 className="size-8 animate-spin text-zinc-800" />
-          <h3 className="text-xl font-semibold">Setting up your account...</h3>
-          <p>You will be redirected automatically</p>
-        </div>
+    <div className="mt-24 flex w-full justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="size-8 animate-spin text-zinc-800" />
+        <h3 className="text-xl font-semibold">Setting up your account...</h3>
+        <p>You will be redirected automatically</p>
       </div>
-    </Suspense>
+    </div>
   )
 }
 
