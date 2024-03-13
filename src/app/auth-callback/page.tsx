2db32be78ad61ef0f71b998389596870cd75c773
dimrev <dimarev444@gmail.com>
2024-03-13 21:22:39 +0200

    Another fix attempt

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useRouter, useSearchParams } from 'next/navigation'
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { trpc } from '../_trpc/client'
 import { Loader2 } from 'lucide-react'
 
@@ -10,21 +10,27 @@ const Page = (props: Props) => {
   const router = useRouter()
   const searchParams = useSearchParams()
 
-  const origin = searchParams.get('origin')
+  const origin = searchParams.get('origin');
+  const [redirecting, setRedirecting] = useState<boolean>(false);
 
   const { data, error } = trpc.authCallback.useQuery(undefined, {
     retry: true,
     retryDelay: 500,
-    onSuccess: (data) => {
-      const success = data.success
-      if (success) router.push(origin ? `/${origin}` : '/dashboard')
-    },
-    onError: (error) => {
-      if (error.data?.code === 'UNAUTHORIZED') router.push('/sign-in')
-    },
-  })
+  });
+
+  if (data?.success) {
+    // Redirect when successful
+    if (!redirecting) {
+      setRedirecting(true);
+      router.push(origin ? `/${origin}` : '/dashboard');
+    }
+  } else if (error?.data?.code === 'UNAUTHORIZED') {
+    // Redirect on error
+    router.push('/sign-in');
+  }
 
   return (
+
     <div className="mt-24 flex w-full justify-center">
       <div className="flex flex-col items-center gap-2">
         <Loader2 className="size-8 animate-spin text-zinc-800" />
@@ -32,6 +38,7 @@ const Page = (props: Props) => {
         <p>You will be redirected automatically</p>
       </div>
     </div>
+
   )
 }
 
