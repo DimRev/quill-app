db4f7eea3021464ebcfb2d68f59b2f9bd627e2b7
dimrev <dimarev444@gmail.com>
2024-03-15 14:59:29 +0200

    Add message submittion logic

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,30 +1,53 @@
-import React from 'react'
+'use client'
+import React, { useContext, useRef } from 'react'
 import { Textarea } from '../ui/textarea'
 import { Button } from '../ui/button'
 import { Send } from 'lucide-react'
+import { ChatContext } from './ChatContext'
 
 type Props = {
   isDisabled?: boolean
 }
 
 const ChatInput = ({ isDisabled }: Props) => {
+  const { addMessage, handleInputChange, isLoading, message } =
+    useContext(ChatContext)
+  const textAreaRef = useRef<HTMLTextAreaElement>(null)
+  const handleSubmit = (ev: React.FormEvent) => {
+    ev.preventDefault()
+    addMessage()
+    textAreaRef.current?.focus()
+  }
   return (
     <div className="absolute bottom-0 left-0 w-full">
-      <form className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+      <form onSubmit={ handleSubmit } className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
         <div className=" relative flex h-full flex-1 items-stretch md:flex-col">
           <div className="relative flex w-full flex-grow flex-col p-4">
             <div className="relative">
               <Textarea
                 autoFocus
-                rows={1}
-                maxRows={4}
+                rows={ 1 }
+                ref={ textAreaRef }
+                maxRows={ 4 }
+                onChange={ handleInputChange }
+                value={ message }
+                onKeyDown={ (ev) => {
+                  if (ev.key === 'Enter' && !ev.shiftKey) {
+                    ev.preventDefault()
+                    addMessage()
+                    textAreaRef.current?.focus()
+                  }
+                } }
                 placeholder="Enter your question..."
                 className="scrollbar-w-2 scrollbar-track-blue-lighter scrollbar-thumb-blue scrollbar-thumb-rounded resize-none py-3 pr-12 text-base"
               />
               <Button
+                disabled={ isLoading || isDisabled }
                 aria-label="send-message"
                 className="absolute bottom-1.5 right-[8px]"
-                size="icon">
+                size="icon"
+                type="submit"
+              >
                 <Send className="size-4" />
               </Button>
             </div>
