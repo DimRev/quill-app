52db8758bfb7b1900695ea98d6cea1dad8ef8421
dimrev <dimarev444@gmail.com>
2024-03-15 07:50:18 +0200

    Prettier format

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -37,16 +37,18 @@ const Dashboard = ({ user }: Props) => {
   return (
     <main className="mx-auto max-w-7xl md:p-10">
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
-        <h1 className="mb-3 text-5xl font-bold text-gray-900">{ user.email } Files</h1>
+        <h1 className="mb-3 text-5xl font-bold text-gray-900">
+          {user.email} Files
+        </h1>
 
         <UploadButton />
       </div>
 
-      {/* Display all user files */ }
+      {/* Display all user files */}
 
-      { files && files.length !== 0 ? (
+      {files && files.length !== 0 ? (
         <ul className="mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3">
-          { files
+          {files
             .sort(
               (a, b) =>
                 new Date(b.createdAt).getTime() -
@@ -54,17 +56,17 @@ const Dashboard = ({ user }: Props) => {
             )
             .map((file) => (
               <li
-                key={ file.id }
+                key={file.id}
                 className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg">
                 <Link
-                  href={ `/dashboard/${file.id}` }
+                  href={`/dashboard/${file.id}`}
                   className="flex flex-col gap-2">
                   <div className="flex w-full items-center justify-between space-x-6 px-6 pt-6">
                     <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
                     <div className="flex-1 truncate">
                       <div className="flex items-center space-x-3">
                         <h3 className="truncate text-lg font-medium text-zinc-900">
-                          { file.name }
+                          {file.name}
                         </h3>
                       </div>
                     </div>
@@ -74,7 +76,7 @@ const Dashboard = ({ user }: Props) => {
                 <div className="mt-4 grid grid-cols-3 place-items-center gap-6 px-6 py-2 text-xs text-zinc-500">
                   <div className="flex items-center gap-2">
                     <Plus className="h-4 w-4" />
-                    { format(new Date(file.createdAt), 'MMM yyyy') }
+                    {format(new Date(file.createdAt), 'MMM yyyy')}
                   </div>
                   <div className="flex items-center gap-2">
                     <MessageSquare className="size-4" />
@@ -84,26 +86,26 @@ const Dashboard = ({ user }: Props) => {
                     size="sm"
                     className="w-full"
                     variant="destructive"
-                    onClick={ () => deleteFile({ id: file.id }) }>
-                    { currentlyDeletingFile === file.id ? (
+                    onClick={() => deleteFile({ id: file.id })}>
+                    {currentlyDeletingFile === file.id ? (
                       <Loader2 className="size-4 animate-spin" />
                     ) : (
                       <TrashIcon className="size-4" />
-                    ) }
+                    )}
                   </Button>
                 </div>
               </li>
-            )) }
+            ))}
         </ul>
       ) : isLoading ? (
-        <Skeleton height={ 100 } className="my-2" count={ 3 } />
+        <Skeleton height={100} className="my-2" count={3} />
       ) : (
         <div className="m-16 flex flex-col items-center gap-2">
           <Ghost className="size-8 text-zinc-800" />
           <h3 className="text-xl font-semibold">Pretty empty around here</h3>
           <p>Let&apos;s upload your first PDF.</p>
         </div>
-      ) }
+      )}
     </main>
   )
 }
