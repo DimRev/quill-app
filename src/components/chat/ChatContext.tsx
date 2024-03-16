90645d4b2c81253006647c5386af3e59ee4b7b10
dimrev <dimarev444@gmail.com>
2024-03-16 04:42:38 +0200

    Add optimistic rendering and stream rendering of messages

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -1,6 +1,8 @@
-import { createContext, useState } from 'react'
+import { createContext, useRef, useState } from 'react'
 import { useToast } from '../ui/use-toast'
 import { useMutation } from '@tanstack/react-query'
+import { trpc } from '@/app/_trpc/client'
+import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query'
 
 type StreamResponse = {
   addMessage: () => void
@@ -25,6 +27,8 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
   const [message, setMessage] = useState<string>('')
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
+  const utils = trpc.useUtils()
+  const backupMessage = useRef('')
   const { toast } = useToast()
 
   const { mutate: sendMessage } = useMutation({
@@ -40,6 +44,148 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
       if (!response.ok) throw new Error('Failed to send message')
 
       return response.body
+    },
+    onMutate: async ({ message }) => {
+      backupMessage.current = message
+      setMessage('')
+
+      // Step 1:
+      await utils.getFileMessages.cancel()
+
+      // Step 2:
+      const previousMessage = utils.getFileMessages.getInfiniteData()
+
+      // Step 3:
+      utils.getFileMessages.setInfiniteData(
+        {
+          fileId,
+          limit: INFINITE_QUERY_LIMIT,
+        },
+        (old) => {
+          if (!old) {
+            return {
+              pages: [],
+              pageParams: [],
+            }
+          }
+
+          let newPages = [...old.pages]
+          let latestPage = newPages[0]!
+
+          latestPage.messages = [
+            {
+              createdAt: new Date().toISOString(),
+              id: crypto.randomUUID(),
+              text: message,
+              isUserMessage: true,
+            },
+            ...latestPage.messages,
+          ]
+
+          newPages[0] = latestPage
+
+          return {
+            ...old,
+            pages: newPages,
+          }
+        }
+      )
+
+      setIsLoading(true)
+
+      return {
+        previousMessages: previousMessage?.pages.flatMap((page) => {
+          return page.messages
+        }),
+      }
+    },
+    onError: (_, __, context) => {
+      setMessage(backupMessage.current)
+      utils.getFileMessages.setData(
+        { fileId },
+        { messages: context?.previousMessages ?? [] }
+      )
+    },
+    onSettled: async () => {
+      setIsLoading(false)
+      await utils.getFileMessages.invalidate({ fileId })
+    },
+    onSuccess: async (stream) => {
+      setIsLoading(false)
+
+      if (!stream) {
+        return toast({
+          title: 'There was a problem sending this message',
+          description: 'Please refresh this page and try again',
+          variant: 'destructive',
+        })
+      }
+
+      const reader = stream.getReader()
+      const decoder = new TextDecoder()
+      let done = false
+
+      // accumulated response
+      let accResponse = ''
+
+      while (!done) {
+        const { value, done: doneReading } = await reader.read()
+        done = doneReading
+        const chunkValue = decoder.decode(value)
+
+        accResponse += chunkValue
+
+        // append chunk to the actual message
+        utils.getFileMessages.setInfiniteData(
+          { fileId, limit: INFINITE_QUERY_LIMIT },
+          (old) => {
+            if (!old) return { pages: [], pageParams: [] }
+
+            let isAiResponseCreated = old.pages.some((page) => {
+              return page.messages.some((message) => {
+                return message.id === 'ai-response'
+              })
+            })
+
+            let updatedPages = old.pages.map((page) => {
+              if (page === old.pages[0]) {
+                let updatedMessages
+
+                if (!isAiResponseCreated) {
+                  updatedMessages = [
+                    {
+                      createdAt: new Date().toDateString(),
+                      id: 'ai-response',
+                      text: accResponse,
+                      isUserMessage: false,
+                    },
+                    ...page.messages,
+                  ]
+                } else {
+                  updatedMessages = page.messages.map((message) => {
+                    if (message.id === 'ai-response') {
+                      return {
+                        ...message,
+                        test: accResponse,
+                      }
+                    }
+                    return message
+                  })
+                }
+
+                return {
+                  ...page,
+                  messages: updatedMessages,
+                }
+              }
+
+              return page
+            })
+
+            return { ...old, pages: updatedPages }
+          }
+        )
+      }
     },
   })
 
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -2,15 +2,18 @@ import { trpc } from '@/app/_trpc/client'
 import { INFINITE_QUERY_LIMIT } from '@/config/infinite-query'
 import { keepPreviousData } from '@tanstack/react-query'
 import { Loader2, MessageSquare } from 'lucide-react'
-import React from 'react'
+import React, { useContext } from 'react'
 import Skeleton from 'react-loading-skeleton'
 import Message from './Message'
+import { ChatContext } from './ChatContext'
 
 type Props = {
   fileId: string
 }
 
 const Messages = ({ fileId }: Props) => {
+  const { isLoading: isAiThinking } = useContext(ChatContext)
+
   const { data, isLoading, fetchNextPage } =
     trpc.getFileMessages.useInfiniteQuery(
       {
@@ -31,20 +34,20 @@ const Messages = ({ fileId }: Props) => {
     id: 'loading-message',
     isUserMessage: false,
     text: (
-      <span className='flex h-full items-center justify-center'>
-        <Loader2 className='h-4 w-4 animate-spin' />
+      <span className="flex h-full items-center justify-center">
+        <Loader2 className="h-4 w-4 animate-spin" />
       </span>
     ),
   }
 
   const combinedMessages = [
-    ...(true ? [loadingMessage] : []),
+    ...(isAiThinking ? [loadingMessage] : []),
     ...(messages ?? []),
   ]
 
   return (
-    <div className='flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'>
-      { combinedMessages && combinedMessages.length > 0 ? (
+    <div className="scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col-reverse gap-4 overflow-y-auto border-zinc-200 p-3">
+      {combinedMessages && combinedMessages.length > 0 ? (
         combinedMessages.map((message, i) => {
           const isNextMessageSamePerson =
             combinedMessages[i - 1]?.isUserMessage ===
@@ -53,42 +56,36 @@ const Messages = ({ fileId }: Props) => {
           if (i === combinedMessages.length - 1) {
             return (
               <Message
-                message={ message }
-                isNextMessageSamePerson={
-                  isNextMessageSamePerson
-                }
-                key={ message.id }
+                message={message}
+                isNextMessageSamePerson={isNextMessageSamePerson}
+                key={message.id}
               />
             )
           } else
             return (
               <Message
-                message={ message }
-                isNextMessageSamePerson={
-                  isNextMessageSamePerson
-                }
-                key={ message.id }
+                message={message}
+                isNextMessageSamePerson={isNextMessageSamePerson}
+                key={message.id}
               />
             )
         })
       ) : isLoading ? (
-        <div className='w-full flex flex-col gap-2'>
-          <Skeleton className='h-16' />
-          <Skeleton className='h-16' />
-          <Skeleton className='h-16' />
-          <Skeleton className='h-16' />
+        <div className="flex w-full flex-col gap-2">
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
+          <Skeleton className="h-16" />
         </div>
       ) : (
-        <div className='flex-1 flex flex-col items-center justify-center gap-2'>
-          <MessageSquare className='h-8 w-8 text-blue-500' />
-          <h3 className='font-semibold text-xl'>
-            You&apos;re all set!
-          </h3>
-          <p className='text-zinc-500 text-sm'>
+        <div className="flex flex-1 flex-col items-center justify-center gap-2">
+          <MessageSquare className="h-8 w-8 text-blue-500" />
+          <h3 className="text-xl font-semibold">You&apos;re all set!</h3>
+          <p className="text-sm text-zinc-500">
             Ask your first question to get started.
           </p>
         </div>
-      ) }
+      )}
     </div>
   )
 }
